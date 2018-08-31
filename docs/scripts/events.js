907b1601fe1187445efe34e3591be7681d50e6bf
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-31 11:12:59 -0500

    sanitize player name inputs
    resolve #3

--- docs/scripts/events.js
+++ docs/scripts/events.js
@@ -2,18 +2,28 @@ function onBtnGoClicked () {
     // check team size is selected
     var teamSize = parseInt($("#team-size option:selected").val());
     if (isNaN(teamSize)) {
-        alert("Error: Please select a team size.");
+        showNoTeamSizeSelectedError();
         return;
     }
 
     // check players list is defined
     var playersInput = $.trim($("#input").val());
     if (!playersInput) {
-        alert("Error: Please give a list of players, separated by Enter");
+        showBlankPlayersListError();
         return;
     }
+
+    var playersList = playersInput.split("\n");
+
+    var sanitizedPlayersInput = []
+    for (var i = 0; i < playersList.length; i++) {
+        var thisPlayer = playersList[i].trim();
+        if (thisPlayer !== "") {
+            sanitizedPlayersInput.push(sanitizePlayerName(thisPlayer));
+        }
+    }
     
-    var randoms = randomize($("#input").val().split("\n"));
+    var randoms = randomize(sanitizedPlayersInput);
     var teams   = splitTeams(randoms, teamSize);
     $("html").trigger("randomized", { teams: teams, players: randoms });
 };
