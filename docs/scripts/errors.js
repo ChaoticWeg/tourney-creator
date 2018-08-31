c37dc939194e1f90c725bcf056928fab5cea37a5
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-31 11:12:28 -0500

    add various error handlers

---
+++ docs/scripts/errors.js
@@ -0,0 +1,7 @@
+function showBlankPlayersListError () {
+    alert("Error: Please give a list of players, separated by Enter.")
+};
+
+function showNoTeamSizeSelectedError () {
+    alert("Error: Please select a team size.")
+};
