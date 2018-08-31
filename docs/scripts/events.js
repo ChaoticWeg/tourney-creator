3db515c792959ceca13090ba5f73bf28cd12101a
ChaoticWeg <shawn@chaoticweg.cc>
2018-08-31 10:57:58 -0500

    add players check
    resolve #7

--- docs/scripts/events.js
+++ docs/scripts/events.js
@@ -1,7 +1,15 @@
 function onBtnGoClicked () {
+    // check team size is selected
     var teamSize = parseInt($("#team-size option:selected").val());
     if (isNaN(teamSize)) {
         alert("Error: Please select a team size.");
+        return;
+    }
+
+    // check players list is defined
+    var playersInput = $.trim($("#input").val());
+    if (!playersInput) {
+        alert("Error: Please give a list of players, separated by Enter");
         return;
     }
     
